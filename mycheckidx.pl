19b32d94a220a6169412deb5ffd0c13fed219ec6
Luke Hudson <lukeletters@gmail.com>
2011-09-25 15:41:53 -0500

    UPDATE to check foreign keys.  NB: This is based on CakePHP naming conventions, your milage may vary

--- mycheckidx.pl
+++ mycheckidx.pl
@@ -36,14 +36,29 @@ my $mydb = DBI->connect("DBI:mysql:database=$opt{d};host=$opt{H}",
 my $tables = $mydb->selectcol_arrayref(q{ SHOW TABLES });
 #print Dumper(\$tables);
 
+print "# SQL commands listed are suggestions only!\n";
+
 for my $table (@$tables) {
 	my $indexes = $mydb->selectall_hashref(qq{ SHOW INDEX FROM `$table` }, 'Key_name');
 	unless($indexes) {
 		print "** TABLE $table is missing indexes\n";
 	} elsif (!$indexes->{PRIMARY}) {
 		print "** TABLE $table is missing PRIMARY KEY\n";
+		print "ALTER TABLE `$table` ADD PRIMARY KEY (id);\n";
 	} else {
 		print "# $table is OK\n";
+	}
+	## Look for *_id fields and check for FOREIGN KEY indexes (Specially for CakePHP)
+	my $fields = $mydb->selectall_hashref(qq{ SHOW FIELDS FROM `$table` WHERE Field LIKE '%\\_id' }, 'Field');
+	for my $field (keys %$fields) {
+		my $guesstable = $field;
+		$guesstable =~ s/_id//;
+		$guesstable =~ s/y$/ies/g;
+		$guesstable =~ s/([^s])$/$1s/g;
+		unless ($indexes->{$field}) {
+			print "** TABLE $table may be missing a foreign key for column $field\n";
+			print "ALTER TABLE `$table` ADD FOREIGN KEY ($field) REFERENCES $guesstable (id);\n";
+		}
 	}
 }
 
